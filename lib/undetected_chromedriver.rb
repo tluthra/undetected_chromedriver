ec9cdcbc01bc7c267efddd33b3ddc788ef1bdf82
Tanooj Luthra <tanoojl@gmail.com>
2023-10-17 18:51:41 -0400

    init

---
+++ lib/undetected_chromedriver.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "undetected_chromedriver/version"
+
+module UndetectedChromedriver
+  class Error < StandardError; end
+  # Your code goes here...
+end
